71722c2bc71673177cda132551a2c5c54a28e1e3
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-04-06 09:36:26 -0300

    Projeto criado

---
+++ src/test/java/com/lucasraimundo/safeway/SafeWayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasraimundo.safeway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeWayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
